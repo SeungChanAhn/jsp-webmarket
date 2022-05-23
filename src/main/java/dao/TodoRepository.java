1e446b932ad88c152def3625baa61ce2ef41cc2c
SeungChan Ahn <102018797+SeungChanAhn@users.noreply.github.com>
2022-05-23 14:05:45 +0900

    Todo 추가 로직 구현 중

---
+++ src/main/java/dao/TodoRepository.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import dto.Todo;
+
+public class TodoRepository { // 저장소는 새로고침해도 계속 써야한다.
+	
+	private static long currentId = 0;
+	
+	private List<Todo> todos = new ArrayList<>();
+	
+	private static TodoRepository instance = new TodoRepository(); // 프로그램이 기동하자마자 만들고 다른애들이 쓸수있게 해줌
+	
+	public static TodoRepository getInstance() {
+		return instance;
+	}
+	
+
+	
+	public List<Todo> getTodos() {
+		return todos;
+	}
+	
+	public void addTodo(Todo todo) {
+		
+		todo.setId(currentId);
+		currentId++;
+		todos.add(todo);
+	}
+}
---
+++ src/main/java/dto/Todo.java
@@ -0,0 +1,41 @@
+package dto;
+
+public class Todo {
+	private long id;// 모든 데이터는 고유한 아이디를 들고있는 것이 좋다.
+					// 각 아이템을 판별할 때 ID를 알면 판별하기 쉬움
+	private String task;
+	private boolean isDone; // 기본값 False
+	
+	public Todo(String task) {
+		this.task = task;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public boolean isDone() {
+		return isDone;
+	}
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", task=" + task + ", isDone=" + isDone + "]";
+	}
+	
+	
+	
+	
+	
+}
