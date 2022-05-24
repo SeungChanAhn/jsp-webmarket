94e0b66ef68a650a28738110ae5d2a241b97a1d5
SeungChan Ahn <102018797+SeungChanAhn@users.noreply.github.com>
2022-05-24 16:21:42 +0900

    Json돌려주는 예시

--- src/main/java/Controller/TodoController.java
+++ src/main/java/Controller/TodoController.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 // 톰캣에 컨트롤러의 존재를 노출
 @WebServlet(name = "TodoController",
-        urlPatterns = {"/addTodo.do", "/toggleTodo.do", "/removeTodo.do"})
+        urlPatterns = {"/addTodo.do", "/toggleTodo.do", "/removeTodo.do", "/todos"})
 public class TodoController extends HttpServlet {
     private static final long serialVersionUID = -5789298759459019056L;
 
@@ -22,8 +22,17 @@ public class TodoController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doGet(req, resp);
+		
+		String command = req.getRequestURI().substring(req.getContextPath().length());
+		
+		TodoRepository repository = TodoRepository.getInstance();
+        if (command.equals("/todos")) {
+            // repository => Json
+            resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            resp.setHeader("Access-Control-Allow-Origin", "*");
+            resp.setContentType("application/json");
+            resp.getWriter().print("{ \"id\": 0, \"task\": \"빨래\" }");
+        }
     }
 	
     @Override
